15413fec72f58344b42cdc35601966d7aff4aba9
Clement <clemillard@gmail.com>
2020-12-02 12:00:25 +0100

    Fix Swipe

--- app/src/main/java/com/example/tindergathering/ui/inscription/Inscription.java
+++ app/src/main/java/com/example/tindergathering/ui/inscription/Inscription.java
@@ -3,5 +3,5 @@ package com.example.tindergathering.ui.inscription;
 public class Inscription {
     private String email;
     private String password;
-    
+
 }
--- app/src/main/java/com/example/tindergathering/ui/swipe/Swipe.java
+++ app/src/main/java/com/example/tindergathering/ui/swipe/Swipe.java
@@ -1,8 +1,21 @@
 package com.example.tindergathering.ui.swipe;
 
-import com.example.tindergathering.ui.utilisateur.Utilisateur;
+import com.example.tindergathering.ui.user.User;
 
 public class Swipe {
-    private Utilisateur user;
-    private Utilisateur newUser;
+    private User user;
+
+    public Swipe() {
+        this.user = null;
+    }
+
+    User findUser(){
+        return new User();
+    }
+    boolean acceptUser(User user){
+        return false;
+    }
+    boolean refuseUser(User user){
+        return false;
+    }
 }
