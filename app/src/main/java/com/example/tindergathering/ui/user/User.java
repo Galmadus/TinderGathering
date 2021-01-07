b08bdd40dc6192a6b84f37428bd86093ca670020
Hugo <hugo.robert2.auditeur@lecnam.net>
2021-01-07 17:16:17 +0100

    Ajout attributs / getter / setter classe user

--- app/src/main/java/com/example/tindergathering/ui/user/User.java
+++ app/src/main/java/com/example/tindergathering/ui/user/User.java
@@ -1,4 +1,51 @@
 package com.example.tindergathering.ui.user;
 
+import java.util.Date;
+
 public class User {
+    private String username;
+    private String password;
+    private Date birthday;
+    private String sexe;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
