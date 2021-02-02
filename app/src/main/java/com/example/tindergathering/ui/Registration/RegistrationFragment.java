03e6a130ce6691149b9831ced15cd2c5b71ec25f
Galmadus <clemillard@gmail.com>
2021-02-02 17:07:48 +0100

    Create Registration Files

--- app/src/main/java/com/example/tindergathering/ui/Registration/RegistrationFragment.java
+++ app/src/main/java/com/example/tindergathering/ui/Registration/RegistrationFragment.java
@@ -23,13 +23,7 @@ public class RegistrationFragment extends Fragment {
         matchsViewModel =
                 ViewModelProviders.of(this).get(RegistrationViewModel.class);
         View root = inflater.inflate(R.layout.fragment_matchs, container, false);
-        final TextView textView = root.findViewById(R.id.text_matchs);
-        matchsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        // final TextView textView = root.findViewById(R.id.text_matchs);
         return root;
     }
 }
