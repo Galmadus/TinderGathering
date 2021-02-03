0d4ffe0a4426e58bf249b6d38ce73ef1ca572bbf
Galmadus <clemillard@gmail.com>
2021-02-03 09:34:12 +0100

    Save the AuthToken in method register

--- app/src/main/java/com/example/tindergathering/ui/Registration/Registration.java
+++ app/src/main/java/com/example/tindergathering/ui/Registration/Registration.java
@@ -3,6 +3,7 @@ package com.example.tindergathering.ui.Registration;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.example.tindergathering.R;
@@ -10,10 +11,14 @@ import com.example.tindergathering.ui.Network;
 
 import java.io.IOException;
 
+import static android.provider.Settings.Global.getString;
+
+
 public class Registration {
     private String email;
     private String password;
     private Context context;
+
     private static final String TAG = "Registration";
 
     public Registration(Context context, String email, String password){
@@ -32,16 +37,25 @@ public class Registration {
                 Network network = new Network(context);
                 String url = context.getResources().getString(R.string.url_start) + "test.json";
                 try {
+                    // API Request
                     String result = network.downloadUrl(url);
                     Log.v(TAG, result.toString());
+
+                    //this.context.getSharedPreferences("AuthToken", Context.MODE_PRIVATE);
+
+                    // Save the new value of Authentification Token
+                    SharedPreferences sharedPref = context.getSharedPreferences("com.example.tindergathering", Context.MODE_PRIVATE);
+                    sharedPref.edit().putString("AuthToken", result).apply();
+
+                    Log.v(TAG, sharedPref.getString("AuthToken", null) );
 
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 return null;
             }
+
         }.execute();
-        //this.context.getSharedPreferences("AuthToken", Context.MODE_PRIVATE);
         return true;
     }
 }
